fc2e16b104716e441f1880862aa965b6c8e61b9f
Daniel Parnell McCarter <dparnellm@gmail.com>
2021-08-12 01:12:21 -0400

    Rename title of page and only change message if validation passes

--- script.js
+++ script.js
@@ -7,12 +7,22 @@ function compute()
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
 	
-	validatePrincipal();
+	// Perform validation for principal.
+	var principalElement = document.getElementById("principal");
+	var principalElementValue = principalElement.value;
+	if ((principalElementValue == 0) || (principalElementValue < 0)) {
+		alert('Show a positive number');
+		principalElement.focus();
+	} 
 	
-	document.getElementById("result").innerHTML = "If you deposit <span class='numbers'>" + principal 
-	+ "</span>,</br>at an interest rate of <span class='numbers'>" + rate 
-	+ "%</span>,</br>You will receive an amount of <span class='numbers'>" + interest 
-	+ "</span>,</br>in the year <span class='numbers'>" + year + "</span";
+	// Display message to user.
+	else {
+		document.getElementById("result").innerHTML = "If you deposit <span class='numbers'>" + principal 
+			+ "</span>,</br>at an interest rate of <span class='numbers'>" + rate 
+			+ "%</span>,</br>You will receive an amount of <span class='numbers'>" + interest 
+			+ "</span>,</br>in the year <span class='numbers'>" + year + "</span";
+	}
+	
 }
 
 // Update the text that indicates what interest rate was selected using the slider.
@@ -21,13 +31,4 @@ function updateRate() {
     document.getElementById("rate_val").innerText = rateval + "%";
 }
 
-// Alert user if they entered 0 or a negative number and give focus to the field again.
-function validatePrincipal() {
-	var principalElement = document.getElementById("principal");
-	var principalElementValue = principalElement.value;
-	if ((principalElementValue == 0) || (principalElementValue < 0)) {
-		alert('Show a positive number');
-		principalElement.focus();
-	}
-}
         
