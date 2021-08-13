e5b863e766debb78197c89cd5df2a6bfc3a564e2
Daniel Parnell McCarter <dparnellm@gmail.com>
2021-08-12 22:43:23 -0400

    Make improvements in formatting at least

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function compute()
 // Update the text that indicates what interest rate was selected using the slider.
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval + "%";
+    document.getElementById("rate_val").innerText = "  " + rateval + "%";
 }
 
         
